7b9a81870b746b913ccc1628841a8373d5220470
B4xim <bk6549@srmist.edu.in>
2025-07-06 17:04:44 +0530

    Blog add, display and edit feature

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+const allBlogs = [];
 
 app.use(express.urlencoded({extended: true}));
 
@@ -10,8 +11,31 @@ app.get("/", (req, res)=>{
 })
 app.post("/submit", (req, res)=>{
     const blog = req.body.blog;
-    res.render("blogs.ejs", {blog});
+    const author = req.body.author;
+    allBlogs.push({author, blog});
+    res.redirect("/blogs")
 })
+app.get("/blogs", (req, res)=>{
+    res.render("blogs.ejs", {allBlogs})
+})
+app.get("/blogs/edit/:id", (req, res)=>{
+    const blogToEdit = allBlogs[req.params.id];
+    res.render("edit.ejs", {blog: blogToEdit, id: req.params.id});
+})
+app.post("/blogs/update/:id", (req, res) => {
+    const index = req.params.id;
+  
+    const updatedAuthor = req.body.author;
+    const updatedContent = req.body.blog;
+
+    allBlogs[index] = {
+      author: updatedAuthor,
+      blog: updatedContent
+    };
+  
+    res.redirect("/blogs");
+  });
+  
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`)    
